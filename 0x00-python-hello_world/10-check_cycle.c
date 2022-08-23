a748059782bd199ec597c643165285bf380e01cd
Diab <odimayodavid7@gmail.com>
2022-08-23 01:37:35 +0100

    Modified

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,7 +4,7 @@
 
 /**
  * check_cycle - Checks if a singly-linked list contains a cycle.
- * @list: A singly-linked list.
+ * @list: A singly-linked list..
  *
  * Return: If there is no cycle - 0.
  *         If there is a cycle - 1.
