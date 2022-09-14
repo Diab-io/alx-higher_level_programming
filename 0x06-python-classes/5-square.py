e60ebbf9b62cccf550ce0fe00e015f480a14f83b
Odimayo David <odimayodavid7@gmail.com>
2022-09-13 20:05:37 -0400

    Updated functionality of size = 0

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,11 +30,11 @@ class Square:
     def my_print(self):
         """ Print the square with the # character. """
         area = self.area()
+        if self.__size == 0:
+            print("")
         for i in range(0, area):
             if i % self.__size == 0:
                 print("\n#", end='')
             else:
                 print("#", end='')
         print("")
-        if self.__size == 0:
-            print("")
