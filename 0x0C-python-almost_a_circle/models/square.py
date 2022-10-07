6fab875a46aa0e483a94b2645c59b7497919520e
Odimayo David <odimayodavid7@gmail.com>
2022-10-07 14:01:21 +0100

    Created square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ A class that inherits from rectangle class """
 
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
