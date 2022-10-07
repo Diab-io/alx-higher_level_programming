6638a095db0a1db34c388d90849d9f9c9ad4b8df
Odimayo David <odimayodavid7@gmail.com>
2022-10-07 23:12:50 +0100

    Added to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Defines a base model class """
+import json
 
 class Base:
     """
@@ -19,3 +20,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns json strong representation """
+        if (list_dictionaries == None or list_dictionaries == ""):
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
