0b011b404ed1c6a034b4fd29572d89e4fd0d390c
Odimayo David <odimayodavid7@gmail.com>
2022-10-07 14:13:12 +0100

    Made it executable

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ Represents a square """
     def __init__(self, size, x=0, y=0, id=None):
+        """Initializing square class"""
         super().__init__(size, size, x, y, id)
     
     def __str__(self):
